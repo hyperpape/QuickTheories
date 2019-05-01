27191a983f9f4aa8f25b109d8b0d91cc750ca74d
Justin Blank <justin.blank@gmail.com>
2019-05-01 17:30:00 -0400

    (feat) Extend AutoRunner to handle 2/3/4 argument methods

--- core/src/main/java/org/quicktheories/impl/AutoRunner.java
+++ core/src/main/java/org/quicktheories/impl/AutoRunner.java
@@ -1,9 +1,13 @@
 package org.quicktheories.impl;
 
-import org.quicktheories.api.Pair;
+import org.quicktheories.QuickTheory;
+import org.quicktheories.api.*;
 import org.quicktheories.core.Configuration;
 import org.quicktheories.core.Gen;
 import org.quicktheories.core.Strategy;
+import org.quicktheories.dsl.TheoryBuilder2;
+import org.quicktheories.dsl.TheoryBuilder3;
+import org.quicktheories.dsl.TheoryBuilder4;
 import org.quicktheories.generators.BooleansDSL;
 import org.quicktheories.generators.IntegersDSL;
 import org.quicktheories.generators.LongsDSL;
@@ -16,7 +20,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Runs an exploration of a method, automatically providing generators for its arguments.
@@ -51,42 +57,128 @@ public class AutoRunner {
         }
 
         if (argumentTypes.size() == 1) {
-            Gen<Object> gen = (Gen<Object>) mkGenerator(argumentTypes.get(0));
-            TheoryRunner<Object, Object> runner = new TheoryRunner<Object, Object>(state, gen, identity(), Object::toString);
-            Function<Object, Object> application = (s) -> {
-                try {
-                    if (isStatic) {
-                        return method.invoke(null, s);
-                    }
-                    else {
-                        return method.invoke(constructor.newInstance(), s);
-                    }
-                }
-                catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            };
-            return runner.check((s) -> {
-                application.apply(s);
-                return true;
-            }, application);
-//        } else if (argumentClasses.size() == 2) {
-//            TheoryBuilder2<?, ?> theoryBuilder = QuickTheory.qt().forAll(mkGenerator(argumentTypes.get(0)), mkGenerator(argumentTypes.get(1)));
-//        } else if (argumentClasses.size() == 3) {
-//            Gen<?> gen1 = mkGenerator(argumentTypes.get(0));
-//            Gen<?> gen2 = mkGenerator(argumentTypes.get(1));
-//            Gen<?> gen3 = mkGenerator(argumentTypes.get(2));
-//            TheoryBuilder3<?, ?, ?> theoryBuilder = QuickTheory.qt().forAll(gen1, gen2, gen3);
-//        } else if (argumentClasses.size() == 4) {
-//            Gen<?> gen1 = mkGenerator(argumentTypes.get(0));
-//            Gen<?> gen2 = mkGenerator(argumentTypes.get(1));
-//            Gen<?> gen3 = mkGenerator(argumentTypes.get(2));
-//            Gen<?> gen4 = mkGenerator(argumentTypes.get(3));
-//            TheoryBuilder4<?, ?, ?, ?> theoryBuilder = QuickTheory.qt().forAll(gen1, gen2, gen3, gen4);
-        } else {
+            return run1Arg(method, state, isStatic, constructor);
+        } else if (argumentClasses.size() == 2) {
+            return run2Args(method, isStatic, constructor);
+        } else if (argumentClasses.size() == 3) {
+            return run3Args(method, isStatic, constructor);
+        } else if (argumentClasses.size() == 4) {
+            return run4Args(method, isStatic, constructor);
+        }
+        else {
             throw new IllegalArgumentException("Too many argument classes");
         }
     }
+
+    private List<Pair<Object, Object>> run1Arg(Method method, Strategy state, boolean isStatic, Constructor<?> constructor) {
+        Gen<Object> gen = mkGenerator(argumentTypes.get(0));
+        TheoryRunner<Object, Object> runner = new TheoryRunner<>(state, gen, identity(), Object::toString);
+        Function<Object, Object> application = (s) -> {
+            try {
+                if (isStatic) {
+                    return method.invoke(null, s);
+                }
+                else {
+                    return method.invoke(constructor.newInstance(), s);
+                }
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+        return runner.check((s) -> {
+            application.apply(s);
+            return true;
+        }, application);
+    }
+
+    private List<Pair<Object, Object>> run2Args(Method method, boolean isStatic, Constructor<?> constructor) {
+        Gen<Object> gen1 = mkGenerator(argumentTypes.get(0));
+        Gen<Object> gen2 = mkGenerator(argumentTypes.get(1));
+        TheoryBuilder2<Object, Object> theoryBuilder = QuickTheory.qt().forAll(gen1, gen2);
+        BiFunction<Object, Object, Object> application = (s1, s2) -> {
+            try {
+                if (isStatic) {
+                    return method.invoke(null, s1, s2);
+                }
+                else {
+                    return method.invoke(constructor.newInstance(), s1, s2);
+                }
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+        List<Pair<Pair<Object, Object>, Object>> valuePairs = theoryBuilder.check((s1, s2) -> {
+            application.apply(s1, s2);
+            return true;
+        }, application);
+        List<Pair<Object, Object>> results = new ArrayList<>();
+        for (Pair<Pair<Object, Object>, Object> result : valuePairs) {
+            results.add(Pair.of(result._1, result._2));
+        }
+        return results;
+    }
+
+    private List<Pair<Object, Object>> run3Args(Method method, boolean isStatic, Constructor<?> constructor) {
+        Gen<Object> gen1 = mkGenerator(argumentTypes.get(0));
+        Gen<Object> gen2 = mkGenerator(argumentTypes.get(1));
+        Gen<Object> gen3 = mkGenerator(argumentTypes.get(2));
+        TheoryBuilder3<Object, Object, Object> theoryBuilder = QuickTheory.qt().forAll(gen1, gen2, gen3);
+        TriFunction<Object, Object, Object, Object> application = (s1, s2, s3) -> {
+            try {
+                if (isStatic) {
+                    return method.invoke(null, s1, s2, s3);
+                }
+                else {
+                    return method.invoke(constructor.newInstance(), s1, s2, s3);
+                }
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+        List<Pair<Tuple3<Object, Object, Object>, Object>> valuePairs = theoryBuilder.check((s1, s2, s3) -> {
+            application.apply(s1, s2, s3);
+            return true;
+        }, application);
+        List<Pair<Object, Object>> results = new ArrayList<>();
+        for (Pair<Tuple3<Object, Object, Object>, Object> result : valuePairs) {
+            results.add(Pair.of(result._1, result._2));
+        }
+        return results;
+    }
+
+    private List<Pair<Object, Object>> run4Args(Method method, boolean isStatic, Constructor<?> constructor) {
+        Gen<Object> gen1 = mkGenerator(argumentTypes.get(0));
+        Gen<Object> gen2 = mkGenerator(argumentTypes.get(1));
+        Gen<Object> gen3 = mkGenerator(argumentTypes.get(2));
+        Gen<Object> gen4 = mkGenerator(argumentTypes.get(3));
+        TheoryBuilder4<Object, Object, Object, Object> theoryBuilder = QuickTheory.qt().forAll(gen1, gen2, gen3, gen4);
+        QuadFunction<Object, Object, Object, Object, Object> application = (s1, s2, s3, s4) -> {
+            try {
+                if (isStatic) {
+                    return method.invoke(null, s1, s2, s3, s4);
+                }
+                else {
+                    return method.invoke(constructor.newInstance(), s1, s2, s3, s4);
+                }
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+        List<Pair<Tuple4<Object, Object, Object, Object>, Object>> valuePairs = theoryBuilder.check((s1, s2, s3, s4) -> {
+            application.apply(s1, s2, s3, s4);
+            return true;
+        }, application);
+        List<Pair<Object, Object>> results = new ArrayList<>();
+        for (Pair<Tuple4<Object, Object, Object, Object>, Object> result : valuePairs) {
+            results.add(Pair.of(result._1, result._2));
+        }
+        return results;
+    }
+
 
     private <T> Function<T,T> identity() {
         return (t) -> t;
@@ -102,25 +194,31 @@ public class AutoRunner {
         return null;
     }
 
-    private Gen<?> mkGenerator(String typeString) {
+    private Gen<Object> mkGenerator(String typeString) {
         return mkSimpleGen(typeString);
     }
 
-    private Gen<?> mkSimpleGen(String typeString) {
+    private Gen<Object> mkSimpleGen(String typeString) {
+        Gen<?> gen;
         switch (typeString) {
             case "java.lang.String":
                 Gen<String> longStrings = new StringsDSL().allPossible().ofLengthBetween(0, 100);
                 Gen<String> shortStrings = new StringsDSL().ascii().ofLengthBetween(0, 10);
-                return shortStrings.mix(longStrings, 10);
+                gen = shortStrings.mix(longStrings, 10);
+                break;
             case "java.lang.Integer":
-                return new IntegersDSL().all();
+                gen = new IntegersDSL().all();
+                break;
             case "java.lang.Long":
-                return new LongsDSL().all();
+                gen = new LongsDSL().all();
+                break;
             case "java.lang.Boolean":
-                return new BooleansDSL().all();
+                gen = new BooleansDSL().all();
+                break;
             default:
                 throw new IllegalArgumentException("Unrecognized type (" + typeString + ")");
         }
+        return (Gen<Object>) gen;
     }
 
 }
